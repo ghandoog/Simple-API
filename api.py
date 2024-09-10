dad49b780c6f4c34145d3ca672ea423f47d19a39
ghandoog <ghando.elshrksy@gmail.com>
2024-09-10 23:49:10 +0400

    main API file

---
+++ api.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# default route
+@app.get("/")
+async def root():
+    return {
+            "welcome_message": "Hello hungry Zucky, welcome!",
+            "menu": [
+                {
+                    "burgers": "http://localhost:8000/api/v1/menu/burgers"
+                },
+                {
+                    "sandwiches": "http://localhost:8000/api/v1/menu/sandwiches"
+                }
+            ]
+    };
+
+
+# burgers route
+@app.get("/api/v1/menu/burgers")
+async def burgers():
+    return {
+        "spicy burger": "10$",
+        "cheesy burger": "12$",
+        "Extra cheesy spicy burger": "18$"
+    }
+    
+    
+
+# sandwiches route
+@app.get("/api/v1/menu/sandwiches")
+async def burgers():
+    return {
+        "Egg sandwich": "10$",
+        "cheesy sandwich": "11$",
+        "Chicken sandwich": "13$"
+    }
